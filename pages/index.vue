c48c38c7888ed4d401fb72c434692aee88708707
satokiy <syamada@netprotections.co.jp>
2022-11-30 01:37:00 +0900

    temp nuxt3

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,42 @@
-<template>
-  <div>
-    <button type="button" name="button" @click="getMsg">
-      RailsからAPIを取得する
-    </button>
-    <div v-for="(msg, i) in msgs" :key="i">
-      {{ msg }}
-    </div>
-  </div>
-</template>
+<script setup lang="ts">
+import axios from 'axios'
+import { ref } from 'vue'
 
-<script>
-export default {
-  data() {
-    return {
-      msgs: [],
-    }
-  },
-  methods: {
-    getMsg() {
-      this.$axios.$get('/api/v1/hello').then((res) => this.msgs.push(res))
-    },
-  },
+const users = ref<Array<any>>([])
+// axios.get('/api/v1/users').then((res) => (users.value = res.data))
+const { data } = await axios.get('/api/v1/users')
+console.log(data)
+
+const dateFormat = (date: string) => {
+  const dateTimeFormat = new Intl.DateTimeFormat('ja', {
+    dateStyle: 'medium',
+    timeStyle: 'short',
+  })
+  return dateTimeFormat.format(new Date(date))
 }
 </script>
+
+<template>
+  <div>
+    <h2>Usersテーブルの取得</h2>
+    <table v-if="users.length">
+      <thead>
+        <tr>
+          <th>id</th>
+          <th>name</th>
+          <th>email</th>
+          <th>created_at</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(user, i) in users" :key="`user-${i}`">
+          <td>{{ user.id }}</td>
+          <td>{{ user.name }}</td>
+          <td>{{ user.email }}</td>
+          <!-- <td>{{ dateFormat(user.created_at) }}</td> -->
+        </tr>
+      </tbody>
+    </table>
+    <div v-else>ユーザー取得ができませんでした</div>
+  </div>
+</template>
