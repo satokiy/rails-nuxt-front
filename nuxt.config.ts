15dc8ab52d7d1d44121639db6a425b85b44b7b9a
satokiy <syamada@netprotections.co.jp>
2022-12-04 16:09:09 +0900

    useFetch test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,6 +9,4 @@ export default defineNuxtConfig({
     },
   },
   modules: ["@nuxtjs/tailwindcss"],
-  
-  
 });
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,34 @@
 <template>
-  <div class="tw-w-48">
-    <NavNavbar />
-    <h1>Main Page</h1>
-    <h1 class="text-3xl font-bold">
-      Hello world!
-   </h1>
+  <div>
+    <h1 class="text-3xl text-center font-bold mb-8">Hello world!</h1>
+    <div class="flex justify-center">
+      <button
+        @click="getUser"
+        class="bg-indigo-700 font-semibold text-white py-2 px-4 rounded transform hover:scale-100 hover:bg-red-700 duration-500"
+      >
+        ボタン
+      </button>
+    </div>
+    <h2>User一覧</h2>
+    <p v-if="error">{{ error }}</p>
+    <ul>
+      <li v-for="post in posts" :key="post.id">{{ post.title }}</li>
+    </ul>
   </div>
 </template>
 <script setup>
 definePageMeta({
   layout: false,
 });
-const { count, inc, dec } = useCounter(1);
-const { $hello } = useNuxtApp();
+const { data: posts, error } = await useAsyncData('posts', () =>
+  $fetch('https://jsonplaceholder.typicode.com/posts/')
+);
+console.log(posts)
+
+// const getUser = async () => {
+//   const res = await useFetch("/api/v1/users");
+//   users = res.data.value;
+//   console.log(users);
+//   return users;
+// };
 </script>
